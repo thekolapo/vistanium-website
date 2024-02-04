6eab9fb275be6ad523a66a8991e9fc1037f98b91
Kolapo Oni <onikolapo@gmail.com>
2024-02-04 20:50:09 +0100

    update project images on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -5,51 +5,80 @@
       <h1 class="u-font-h1">Projects</h1>
     </div>
     <flickity ref="flickity" class="home__projects" :options="flickityOptions">
-      <div class="home__project">
-        <img src="@/assets/images/vistanium-mag-img.jpg" alt="" />
+      <a
+        href="https://www.vistanium.com/"
+        target="_blank"
+        class="home__project"
+      >
+        <img
+          src="@/assets/images/vistanium-mag-img.jpg"
+          alt="Vistanium mag image"
+        />
         <p class="u-font-normal">Vistanium Mag</p>
         <p>
           Immersive essays that narrate extraordinary true stories. They provide
           compelling insights into life in various cities, offering readers a
           rich, multifaceted perspective.
         </p>
-      </div>
-      <div class="home__project">
-        <img src="@/assets/images/vistanium-mag-img.jpg" alt="" />
+      </a>
+      <a
+        href="https://twentysevenproductions.co/itoosabi/"
+        target="_blank"
+        class="home__project"
+      >
+        <img src="@/assets/images/i-too-sabi.jpg" alt="I too sabi image" />
         <p class="u-font-normal">I too sabi</p>
         <p>
           A Nigeria-focused conversational "ask me anything" podcast that
           answers a wide range of questions through research and expert
           insights.
         </p>
-      </div>
-      <div class="home__project">
-        <img src="@/assets/images/vistanium-mag-img.jpg" alt="" />
+      </a>
+      <a
+        href="https://www.sunandcountry.com/"
+        target="_blank"
+        class="home__project"
+      >
+        <img
+          src="@/assets/images/sun-and-country.jpg"
+          alt="Sun and Country image"
+        />
         <p class="u-font-normal">Sun and Country</p>
         <p>
           A story-led guide to understanding the Nigerian Civil War, capturing
           the events that led to it, how it played out on all sides, and how it
           continues to shape our world today.
         </p>
-      </div>
-      <div class="home__project">
-        <img src="@/assets/images/vistanium-mag-img.jpg" alt="" />
+      </a>
+      <a
+        href="https://twentysevenproductions.co/itoosabi/"
+        target="_blank"
+        class="home__project"
+      >
+        <img src="@/assets/images/i-too-sabi.jpg" alt="I like girls image" />
         <p class="u-font-normal">I like girls</p>
         <p>
           A documentary-style, narrative-driven podcast exploring the diverse
           stories of African women. It offers firsthand insights into how gender
           shapes women's lives.
         </p>
-      </div>
-      <div class="home__project">
-        <img src="@/assets/images/vistanium-mag-img.jpg" alt="" />
-        <p class="u-font-normal">Vistanium Mag</p>
+      </a>
+      <a
+        href="https://www.sunandcountry.com/"
+        target="_blank"
+        class="home__project"
+      >
+        <img
+          src="@/assets/images/sun-and-country.jpg"
+          alt="Sun and Country image"
+        />
+        <p class="u-font-normal">Sun and Country</p>
         <p>
-          Immersive essays that narrate extraordinary true stories. They provide
-          compelling insights into life in various cities, offering readers a
-          rich, multifaceted perspective.
+          A story-led guide to understanding the Nigerian Civil War, capturing
+          the events that led to it, how it played out on all sides, and how it
+          continues to shape our world today.
         </p>
-      </div>
+      </a>
     </flickity>
     <div class="home__collaborations">
       <p class="u-font-normal home__collaborations-subtext">
@@ -91,8 +120,6 @@ export default {
 
 <style lang="scss" scoped>
 .home {
-  margin-top: 20rem;
-
   &__projects {
     position: relative;
     width: 100vw;
@@ -103,6 +130,8 @@ export default {
   &__project {
     width: 25%;
     margin-right: 5rem;
+    text-decoration: none;
+    color: unset;
 
     img {
       height: 40rem;
